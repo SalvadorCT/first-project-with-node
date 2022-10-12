edfea6c44d252b90e542692c6eaea7b899d25eb9
SalvadorCT <salvadorlinc@gmail.com>
2022-10-11 19:05:49 -0500

    update and add files 11/10/22

---
+++ 10-tareas-por-hacer/app.js
@@ -0,0 +1,45 @@
+
+require('colors');
+
+const { 
+    inquirerMenu, 
+    pausa,
+    leerInput,
+} = require('./helpers/inquirer');
+
+const Tareas = require('./models/tareas');
+// const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+
+const main = async() => {
+
+    let opt = '';
+    const tareas = new Tareas();
+
+    do {
+        // Imprimir el menú
+        opt = await inquirerMenu();
+        // console.log({opt});
+        switch (opt) {
+            case '1':
+                // Crear opción
+                const desc = await leerInput('Descripción: ');
+                tareas.crearTarea(desc);
+            break;
+            case '2':
+                console.log(tareas.listadoArr);
+            break;
+        }
+
+        await pausa();
+
+    } while (opt !== '0');
+
+}
+
+main();
+
+
+
+
+
